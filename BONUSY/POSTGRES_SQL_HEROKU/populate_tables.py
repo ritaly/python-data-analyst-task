ef90b925f02c22dbeb3bfa394044df4f878ab419
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2022-12-27 11:02:30 +0100

    Update populate_tables.py

--- BONUSY/POSTGRES_SQL_HEROKU/populate_tables.py
+++ BONUSY/POSTGRES_SQL_HEROKU/populate_tables.py
@@ -2,5 +2,5 @@ import pandas as pd
 from db_management import populate_table
 
 
-PDSDAT_df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT8rjDwUmmIprIHEJbi1bRN9w83ULhrdtlHoHK7gYkJvdTnJi8smXhrCbTsxc4Sh8iKbDC2kivkKTyI/pub?gid=0&single=true&output=csv")
+PDSDAT_df = pd.read_csv("https://data.heroku.com/dataclips/eqxbztkzhrxglwwandwdezjnyphf.csv")
 populate_table(table_name="PDSDAT", df=PDSDAT_df)
