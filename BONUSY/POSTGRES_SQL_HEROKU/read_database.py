d861d21df2f223b7cc28621d17ac10ec4d9ade2c
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2022-12-27 17:50:24 +0100

    Create read_database.py

---
+++ BONUSY/POSTGRES_SQL_HEROKU/read_database.py
@@ -0,0 +1,18 @@
+import psycopg2
+import os
+
+
+db_connection = psycopg2.connect(
+    host="********************",
+    user="********************",
+    password="********************",
+    dbname="********************",
+)
+
+
+cur = db_connection.cursor()
+cur.execute("SELECT * FROM public.pdsdat LIMIT 884;")
+
+
+print(cur.fetchall())
+cur.close()
