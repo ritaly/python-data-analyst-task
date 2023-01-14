c65f7d2f83f3c2c791308038c39be06934f44814
Luke <86468152+LukeBlueLOx@users.noreply.github.com>
2023-01-14 17:11:06 +0100

    Update import_csv_data_from_api.py

--- BONUSY/import_csv_data_from_api.py
+++ BONUSY/import_csv_data_from_api.py
@@ -1,38 +1,35 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-import time
 from datetime import datetime
+import time
 
-SERVICE_ACCOUNT_FILE = '/home/blox_land/Desktop/SCV2PL/sars-cov-2-poland.json'
+SERVICE_ACCOUNT_FILE = 'json_file_from_google_credentials.json'
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-creds = None
 creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
         
+service = build('sheets', 'v4', credentials=creds)
+
+sheet = service.spreadsheets()        
+
+
+data_from_api = 'https://api.dane.gov.pl/resources/44100,liczba-osob-ktore-przystapiyzday-egzamin-maturalny/file'        
 spreadsheet_id1 = '1ksRYtgapnIE7pOs5OBjk4zFUOndT3p_IUjXWV9rM5BI'
-sheet_id1 = 0
+sheet_id1 = 1256508534
+
 date = datetime.today().strftime('%Y-%m-%d') #'YYYY-MM-DD'
 
 
-service = build('sheets', 'v4', credentials=creds)
-
-sheet = service.spreadsheets()
-
-
-result = sheet.values().get(spreadsheetId=spreadsheet_id1,
-                            range="BONUS!A1").execute()
-values = result.get('values', [])
-
-RUN1 = [['=IMPORTDATA(I1;";")',"","","","","","","","https://api.dane.gov.pl/resources/41967,liczba-osob-ktore-przystapiyzday-egzamin-maturalny/file","data"],
-["","","","","","","","","",str(date)]]
-
+RUN1 = [['=IMPORTDATA("'+str(data_from_api)+'";";")',"","","","","","","","data"],
+        ["","","","","","","","",str(date)]]
 request1 = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id1,
-                                                  range="BONUS!A1",
-                                                  valueInputOption="USER_ENTERED", body={"values": RUN1}).execute()
-time.sleep(10)
+                                                  range="BONUS_SOURCE!A1",
+                                                  valueInputOption="USER_ENTERED",
+                                                  body={"values": RUN1}).execute()
+time.sleep(3)
 print(request1)
-
 
 RUN2 = {'requests': [
     {'copyPaste': {
@@ -55,8 +52,9 @@ RUN2 = {'requests': [
     }},
 
 ]}
-
-request2 = service.spreadsheets().batchUpdate(
-        spreadsheetId=spreadsheet_id1, body=RUN2).execute()
-
+request2 = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id1,
+                                              body=RUN2).execute()
 print(request2)
+
+
+print("(All Operations - Successfully!)")
